7254c8663268ae65bb036e48b8512e1b5e62a138
Torben Döscher <mail@tdoescher.de>
2021-10-22 14:25:05 +0200

    fix DCA

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -1,10 +1,10 @@
 <?php
 
-$GLOBALS['TL_DCA']['tl_content']['palettes']['googlemaps_embed'] = '{type_legend},type,headline;{googlemaps_legend},googlemaps_address,googlemaps_zoom,googlemaps_apikey;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
+$GLOBALS['TL_DCA']['tl_content']['palettes']['googlemaps_embed'] = '{type_legend},type,headline;{googlemaps_legend},googlemaps_apikey,googlemaps_zoom,googlemaps_address;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['googlemaps_address'] = array
+$GLOBALS['TL_DCA']['tl_content']['fields']['googlemaps_apikey'] = array
 (
-    'label'     => &$GLOBALS['TL_LANG']['tl_content']['googlemaps_address'],
+    'label'     => &$GLOBALS['TL_LANG']['tl_content']['googlemaps_apikey'],
     'exclude'   => true,
     'inputType' => 'text',
     'eval'      => array('mandatory' => true, 'tl_class' => 'w50', 'maxlength' => '255'),
@@ -21,9 +21,9 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['googlemaps_zoom'] = array
     'sql'       => "varchar(2) NOT NULL default '16'"
 );
 
-$GLOBALS['TL_DCA']['tl_content']['fields']['googlemaps_apikey'] = array
+$GLOBALS['TL_DCA']['tl_content']['fields']['googlemaps_address'] = array
 (
-    'label'     => &$GLOBALS['TL_LANG']['tl_content']['googlemaps_apikey'],
+    'label'     => &$GLOBALS['TL_LANG']['tl_content']['googlemaps_address'],
     'exclude'   => true,
     'inputType' => 'text',
     'eval'      => array('mandatory' => true, 'tl_class' => 'clr', 'maxlength' => '255'),
