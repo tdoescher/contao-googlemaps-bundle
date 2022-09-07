7e26fe14a4e64601ac7a01cf5460270606fc94e3
Torben Döscher <mail@tdoescher.de>
2022-09-07 12:05:17 +0000

    Update

--- src/GoogleMapsBundle.php
+++ src/GoogleMapsBundle.php
@@ -1,5 +1,14 @@
 <?php
  
+ /**
+  * This file is part of GoogleMaps Bundle for Contao
+  *
+  * @package     tdoescher/googlemaps-bundle
+  * @author      Torben Döscher <mail@tdoescher.de>
+  * @license     LGPL
+  * @copyright   tdoescher.de - WEB und IT <https://tdoescher.de>
+  */
+
 namespace tdoescher\GoogleMapsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
