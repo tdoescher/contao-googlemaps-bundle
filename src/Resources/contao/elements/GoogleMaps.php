f52199af5d90c7cc0254becc95ccd44500fa7cc5
Torben Döscher <mail@tdoescher.de>
2022-09-07 10:09:43 +0000

    added support for Contao 5

--- src/Resources/contao/elements/GoogleMaps.php
+++ src/Resources/contao/elements/GoogleMaps.php
@@ -2,16 +2,22 @@
 
 namespace tdoescher\GoogleMapsBundle;
 
-class GoogleMaps extends \ContentElement
+use Contao\ContentElement;
+use Contao\BackendTemplate;
+use Contao\System;
+
+class GoogleMaps extends ContentElement
 {
     protected $strTemplate = 'ce_googlemaps_embed';
 
     protected function compile()
     {
-        if (TL_MODE == 'BE')
+        $request = System::getContainer()->get('request_stack')->getCurrentRequest();
+
+        if ($request && System::getContainer()->get('contao.routing.scope_matcher')->isBackendRequest($request))
         {
             $this->strTemplate = 'be_wildcard';
-            $this->Template = new \BackendTemplate($this->strTemplate);
+            $this->Template = new BackendTemplate($this->strTemplate);
 
             $this->Template->wildcard = $this->arrData['googlemaps_address'];
         }
