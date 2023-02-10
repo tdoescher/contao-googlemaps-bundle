ed65d35ced6b18f5d54de8b0684e9d1596786264
Torben Döscher <mail@tdoescher.de>
2023-02-10 10:42:20 +0000

    Update to PHP attributes, requires PHP8 and Contao 4.13

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -13,7 +13,6 @@ $GLOBALS['TL_DCA']['tl_content']['palettes']['googlemaps_embed'] = '{type_legend
 $GLOBALS['TL_DCA']['tl_content']['palettes']['googlemaps_html'] = '{type_legend},type,headline;{googlemaps_legend},html;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space;{invisible_legend:hide},invisible,start,stop';
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['googlemaps_apikey'] = [
-(
   'label'     => &$GLOBALS['TL_LANG']['tl_content']['googlemaps_apikey'],
   'exclude'   => true,
   'inputType' => 'text',
@@ -22,7 +21,6 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['googlemaps_apikey'] = [
 ];
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['googlemaps_zoom'] = [
-(
   'label'     => &$GLOBALS['TL_LANG']['tl_content']['googlemaps_zoom'],
   'default'   => 16,
   'exclude'   => true,
@@ -32,7 +30,6 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['googlemaps_zoom'] = [
 ];
 
 $GLOBALS['TL_DCA']['tl_content']['fields']['googlemaps_address'] = [
-(
   'label'     => &$GLOBALS['TL_LANG']['tl_content']['googlemaps_address'],
   'exclude'   => true,
   'inputType' => 'text',
