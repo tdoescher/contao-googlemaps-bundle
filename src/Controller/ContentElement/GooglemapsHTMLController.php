cad30a211579a894b18a092d3ab46549f91b64fd
Torben Döscher <mail@tdoescher.de>
2024-06-18 09:23:02 +0200

    Update

--- src/Controller/ContentElement/GooglemapsHTMLController.php
+++ src/Controller/ContentElement/GooglemapsHTMLController.php
@@ -19,7 +19,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 #[AsContentElement(category:'media')]
-class GooglemapsHtmlController extends AbstractContentElementController
+class GooglemapsHtmlsController extends AbstractContentElementController
 {
   protected function getResponse(FragmentTemplate $template, ContentModel $model, Request $request): Response
   {
